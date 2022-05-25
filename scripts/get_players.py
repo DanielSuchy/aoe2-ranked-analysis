dfdaf795f1c5c9433c37bf81c8a36f88319e68db
DanielSuchy <daniel.suchy@gmail.com>
2022-05-25 20:43:26 +0300

    save data in csv

--- scripts/get_players.py
+++ scripts/get_players.py
@@ -1,7 +1,8 @@
 #download the 1v1 ranked leaderboard from aoe2.net
 import requests
-import json
+import pandas as pd
 
+path = '../data/players.csv'
 game = "aoe2de" #ignore the HD version
 leaderbleaderboard_id = str(3) #download the 1v1 ranked leaderboard
 start = str(1) #start at the highest rated player
@@ -16,7 +17,8 @@ api_url = ("https://aoe2.net/api/leaderboard?" +
 
 response = requests.get(api_url)
 data_in_json = response.json()
+relevant_data = data_in_json["leaderboard"]
 
-#save the result as json
-with open('../data/json/top_players.json', 'w') as outfile:
-    json.dump(data_in_json, outfile, indent = 4) #indent = 4 makes the file human-readable
+#save the result as csv
+df = pd.DataFrame(relevant_data)
+df.to_csv(path, index=False)
